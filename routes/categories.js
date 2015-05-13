f0a1faaea124a488d4692b729959327cf83f11f8
marb <marb61a@gmail.com>
2015-05-13 08:10:39 +0000

    added category creation functionality

---
+++ routes/categories.js
@@ -0,0 +1,46 @@
+var express = require('express');
+var router = express.Router();
+var mongo = require('mongodb');
+var db = require('monk')('localhost/nodeblog');
+
+router.get('/add', function(req, res, next){
+    res.render('addcategory',{
+		"title":"Add Category"
+	});
+});
+
+router.post('/add', function(req, res, next){
+	// Get The Form Values
+	var title = req.body.title;
+
+	// Form Validation
+	req.checkBody('title','Title field is required').notEmpty();
+
+	// Check for Errors
+	var errors = req.validationErrors();
+
+	if(errors){
+		res.render('addcategory',{
+			"errors": errors,
+			"title": title
+		});
+	} else {
+		var categories = db.get('categories');
+
+		// Submit to Database
+		categories.insert({
+			"title": title
+		}, function(err, category){
+			if(err){
+				res.send('There was an issue submitting the category');
+			} else {
+				req.flash('success','Category Submitted');
+				res.location('/');
+				res.redirect('/');
+			}
+		});
+	}
+});
+
+
+module.exports = router;
