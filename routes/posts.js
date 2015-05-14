fb2b0aa9968af3abff198ea68558cc662cf7a89b
marb <marb61a@gmail.com>
2015-05-14 08:54:09 +0000

    Finished project, working perfectly

--- routes/posts.js
+++ routes/posts.js
@@ -3,6 +3,16 @@ var router = express.Router();
 var mongo = require('mongodb');
 var db = require('monk')('localhost/nodeblog');
 
+router.get('/show/:id', function(req, res, next){
+	var posts = db.get('posts');
+	posts.findById(req.params.id, function(err, post){
+		res.render('show',{
+			"post": post
+		});
+	});
+});
+
+
 router.get('/add', function(req, res, next){
 	var categories = db.get('categories');
 
@@ -68,7 +78,62 @@ router.post('/add', function(req, res, next){
 			}
 		});
 	}
-
 });
+
+router.post('/addcomment', function(req, res, next){
+	// Get The Form Values
+	var name = req.body.name;
+	var email = req.body.email;
+	var body = req.body.body;
+	var postid = req.body.postid;
+	var commentdate = new Date();
+
+	// Validation of the form
+	req.checkBody('name','Name field is required').notEmpty();
+	req.checkBody('email','Email field is required').notEmpty();
+	req.checkBody('email','Email is not formatted correctly').isEmail();
+	req.checkBody('body', 'Body field is required').notEmpty();
+
+	// Check Errors
+	var errors = req.validationErrors();
+
+	if(errors){
+		var posts = db.get('posts');
+		posts.findById(postid, function(err, post){
+			res.render('show',{
+				"errors": errors,
+				"post": post
+			});
+		});
+		
+	} else {
+		var comment = {"name": name, 
+						"email": email, 
+						"body": body, 
+						"commentdate": commentdate}
+
+		var posts = db.get('posts');
+
+		posts.update({
+				"_id": postid
+			},
+			{
+				$push:{
+					"comments":comment
+				}
+			},
+			function(err, doc){
+				if(err){
+					throw err;
+				} else {
+					req.flash('success','Comment Added');
+					res.location('/posts/show/'+postid);
+					res.redirect('/posts/show/'+postid);
+				}
+			}
+		);
+	}
+});
+
 
 module.exports = router;
